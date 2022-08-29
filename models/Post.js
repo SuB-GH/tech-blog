b3609826802f5bbae7f4d0ac03eb5f75a57dc8f1
Susan Brown <brown684408@gmail.com>
2022-08-29 13:04:46 -0500

    initial file setup

---
+++ models/Post.js
@@ -0,0 +1,73 @@
+const { Model, DataTypes } = require('sequelize');
+// this is the connection to MySQL we stored in the connection.js file
+const sequelize = require('../config/connection');
+
+// create our Post model - REMOVE VOTE FROM THIS
+// class Post extends Model {
+//   static upvote(body, models) {
+//     return models.Vote.create({
+//       user_id: body.user_id,
+//       post_id: body.post_id
+//     }).then(() => {
+//       return Post.findOne({
+//         where: {
+//           id: body.post_id
+//         },
+//         attributes: [
+//           'id',
+//           'post_url',
+//           'title',
+//           'created_at',
+//           [
+//             sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+//             'vote_count'
+//           ]
+//         ]
+//       });
+//     });
+//   }
+
+// }
+
+// create fields/columns for Post model. Columns are: id, title, post_url and user_id
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    post_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isURL: true // Sequelize offers this as validation that this url is a verified link
+      }
+    },
+    // "userId column determines who posted the article. 
+    //references property establishes link btwn this post and user model (specifically the "id" column defined by key property which is the primary key.
+    // The user_id is conversely defined as the foreign key and will be the matching link.
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    }
+  },
+  // below, we configure the metadata, including the naming conventions
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post'
+  }
+);
+
+//export expression to make the Post model accessible to other parts of the application
+module.exports = Post;
---
+++ models/index.js
@@ -0,0 +1,32 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./comment');
+
+// create associations
+// one-to-many relationship. creates the reference for the id column in the User model to link to the corresponding foreign key pair, which is the user_id in the Post model.
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// reverse of above. this defines relationship of Post to User. Post can belong to one user, but not many.
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+  Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+  });
+  
+  User.hasMany(Comment, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+  });
+
+module.exports = { User, Post, Comment };
